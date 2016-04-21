74bc8f56dd81f002097761856a7f79955ee07d6a
CYBoys <15923456720@163.com>
2016-04-21 09:04:12 +0800

    iOS
    汉字转拼音等

---
+++ ChineseCharacterPinyin/ChineseCharacterPinyin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChineseCharacterPinyin
+//
+//  Created by chairman on 16/3/15.
+//  Copyright © 2016年 LaiYoung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChineseCharacterPinyin/ChineseCharacterPinyin/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  ChineseCharacterPinyin
+//
+//  Created by chairman on 16/3/15.
+//  Copyright © 2016年 LaiYoung. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *hanziText = @"巴南";
+    if ([hanziText length]) {
+        NSMutableString *ms = [[NSMutableString alloc] initWithString:hanziText];
+        if (CFStringTransform((__bridge CFMutableStringRef)ms, 0, kCFStringTransformMandarinLatin, NO)) {
+            NSLog(@"pinyin: %@", ms);
+        }
+        if (CFStringTransform((__bridge CFMutableStringRef)ms, 0, kCFStringTransformStripDiacritics, NO)) {
+            NSLog(@"pinyin: %@", ms);
+        }  
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
